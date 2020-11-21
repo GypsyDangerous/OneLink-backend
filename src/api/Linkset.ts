cea2d5c98da730d13a5f214beb74cb683c295144
David Snyder <davidgraygs4@gmail.com>
2020-11-21 12:15:10 -0500

    begin adding endpoint to add link to page

--- src/api/Linkset.ts
+++ src/api/Linkset.ts
@@ -1,5 +1,7 @@
-import { Router } from "express";
+import { Router, Request } from "express";
+import uid from "uid";
 import LinkSet from "../models/LinkSet.model";
+import { Link, LinkSet as Page } from "../types/LinkSet";
 
 const router = Router();
 
@@ -16,11 +18,33 @@ router.get("/:username", (req, res, next) => {
 router.post("/:username/create", async (req, res, next) => {
 	const linkset = {
 		owner: req.params.username,
-		
 	};
 	const newLinkSet = new LinkSet(linkset);
-	await newLinkSet.save()
+	await newLinkSet.save();
 	res.json({ code: 200, data: newLinkSet });
 });
+
+router.patch(
+	"/:username/add",
+	async (req: Request<{ username: string }, unknown, Link>, res, next) => {
+		const owner = req.params.username;
+		const getUser = (): Promise<Page> => {
+			return new Promise((res, rej) =>
+				LinkSet.findOne({ owner }, (err, doc) => {
+					if (err) {
+						return rej(err);
+					}
+					if (doc) {
+						res(doc);
+					}
+				})
+			);
+		};
+		const linkSet = await getUser();
+		const link = req.body;
+		link.id = uid();
+		linkSet.links.push(link);
+	}
+);
 
 export = router;
