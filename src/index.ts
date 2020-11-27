88fb5a00a3fac5fc67fdd000e5ea9f6ed014e172
David Snyder <davidgraygs4@gmail.com>
2020-11-27 17:21:42 -0500

    change default port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
   
 import app from "./app"
 
-const port = process.env.PORT || 1800;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening on port:${port}`);
