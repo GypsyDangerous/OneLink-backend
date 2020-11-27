c838fb81cb2826c5b97d5531ddd17f0aadedd56e
David Snyder <davidgraygs4@gmail.com>
2020-11-27 12:36:36 -0500

    add custom express request extension

---
+++ src/custom.d.ts
@@ -0,0 +1,5 @@
+declare namespace Express {
+	export interface Request {
+		userData: { userId?: string };
+	}
+}
--- src/types/Request.ts
+++ src/types/Request.ts
@@ -1,13 +1,6 @@
-import { Request } from "express";
-
 export interface Session{
 	userId?: string
 }
-
-export interface AuthRequest extends Request{
-	userData?: Session
-}
-
 export interface UrlBody extends Buffer{
 	url: string
 }
