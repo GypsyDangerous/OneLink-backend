cc65f702cf1ac71ea1b0fa50b764eb58e77c0492
David Snyder <davidgraygs4@gmail.com>
2020-11-30 18:49:36 -0500

    change the way we get the auth token in graphql

--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { checkAuth } from "../utils/functions";
 
 export const context = async ({ req, res }: { req: Request; res: Response }): Promise<any> => {
-	const token = req.headers.authorization || "";
+	const token = req.get("Authorization");
 	const authResult = await checkAuth(token);
 	const id = authResult?.userId;
 	return { id, req, res, setCookies: [], setHeaders: [] };
