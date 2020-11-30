8fc37b2f187b030227557adbcef979beb4b48bda
David Snyder <davidgraygs4@gmail.com>
2020-11-30 18:48:52 -0500

    extend context with more return values

--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -1,9 +1,9 @@
-import { Request } from "express";
+import { Request, Response } from "express";
 import { checkAuth } from "../utils/functions";
 
-export const context = async ({ req }: { req: Request }): Promise<any> => {
-	const token = req.headers.authorization || '';
+export const context = async ({ req, res }: { req: Request; res: Response }): Promise<any> => {
+	const token = req.headers.authorization || "";
 	const authResult = await checkAuth(token);
 	const id = authResult?.userId;
-	return {id}
+	return { id, req, res, setCookies: [], setHeaders: [] };
 };
