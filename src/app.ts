8bb3e8fc24d93d67d49ab7c00f0fc476ec1fa007
David Snyder <davidgraygs4@gmail.com>
2020-11-30 19:50:33 -0500

    add refresh token endpoint

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,18 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers, context } from "./graphql";
-import httpHeadersPlugin from "apollo-server-plugin-http-headers"
+import httpHeadersPlugin from "apollo-server-plugin-http-headers";
+import cookie_parser from "cookie-parser";
+import jwt from "jsonwebtoken";
 dotenv.config();
 
 // const middlewares = require("./middlewares");
 import { notFound, errorHandler } from "./middleware";
 import { upload_path } from "./utils/constants";
 import api from "./api";
+import { payload } from "./types/Auth";
+import { createAuthToken, getRefreshSecret } from "./utils/functions";
+import User from "./models/User.model";
 // const api = require("./api");
 
 const app = express();
@@ -23,6 +28,7 @@ if (process.env.DEBUG_MODE === "true") {
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(cookie_parser());
 
 const uri = process.env.ATLAS_URI || "";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
@@ -31,10 +37,9 @@ connection.once("open", () => {
 	console.log("MongoDB database connection successful");
 });
 
-const server = new ApolloServer({ typeDefs, resolvers, context, plugins: [httpHeadersPlugin], });
+const server = new ApolloServer({ typeDefs, resolvers, context, plugins: [httpHeadersPlugin] });
 
 server.applyMiddleware({ app });
-
 
 app.use(upload_path.substr(1), express.static(upload_path.substr(2)));
 
@@ -42,6 +47,35 @@ app.get("/", (req, res) => {
 	res.json({
 		message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
 	});
+});
+
+app.post("/refresh_token", async (req, res, next) => {
+	const token: string = req.cookies["refresh_token"];
+
+	console.log(token);
+
+	if (!token) {
+		return res.status(401).json({ code: 401, message: "missing refresh token" });
+	}
+
+	try {
+		const payload: payload | string = jwt.verify(token, getRefreshSecret());
+
+		if (typeof payload === "string") throw "bad payload";
+
+		const user = await User.findOne({ _id: payload.userId });
+
+		if (!user) throw "no user";
+		console.log(payload);
+
+		res.json({
+			code: 200,
+			data: { token: createAuthToken({ userId: payload.userId, email: payload.email }) },
+		});
+	} catch (err) {
+		console.log(err.message);
+		res.status(401).json({ code: 401, message: "invalid refresh token" });
+	}
 });
 
 app.use("/v1", api);
