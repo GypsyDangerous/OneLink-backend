1697ef265f456e4918e009fe80cfd59a96e2a2b2
David Snyder <davidgraygs4@gmail.com>
2020-11-30 19:51:23 -0500

    add context interface and format

--- src/types/Request.ts
+++ src/types/Request.ts
@@ -1,6 +1,16 @@
-export interface Session{
-	userId?: string
+import { Request, Response } from "express";
+
+export interface Session {
+	userId?: string;
 }
-export interface UrlBody extends Buffer{
-	url: string
+export interface UrlBody extends Buffer {
+	url: string;
+}
+
+export interface Context {
+	id?: string;
+	req: Request;
+	res: Response;
+	setCookies: any[];
+	setHeaders: any[];
 }
