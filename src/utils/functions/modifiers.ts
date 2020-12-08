220b72151a943231d6920d36b7c6cdde3bf6c502
David Snyder <davidgraygs4@gmail.com>
2020-12-08 07:05:35 -0500

    formatting

--- src/utils/functions/modifiers.ts
+++ src/utils/functions/modifiers.ts
@@ -3,8 +3,8 @@ import { UserModification } from "../../types/User";
 import bcrypt from "bcrypt";
 import { getPage } from "./getters";
 import { Page as Page, Link } from "../../types/Page";
-import User from "../../models/User.model"
-import uid from "uid"
+import User from "../../models/User.model";
+import uid from "uid";
 
 export const updateUser = async (
 	id?: string,
@@ -41,19 +41,23 @@ export const updateUser = async (
 
 export const addLink = async (owner: string, link: Link): Promise<Page> => {
 	const Page = await getPage(owner);
+	if (!Page) throw new Error("invalid page");
+
 	link.id = uid();
 	Page.links.push(link);
-	Page.markModified('links');
-	Page.save();
+	Page.markModified("links");
+	await Page.save();
 	return Page;
 };
 
 export const updateLink = async (owner: string, link: Link): Promise<Page> => {
 	const Page = await getPage(owner);
+	if (!Page) throw new Error("invalid page");
+
 	const linkIndex = Page.links.findIndex(li => li.id === link.id);
 	Page.links[linkIndex] = link;
-	Page.markModified('links');
-	Page.save()
+	Page.markModified("links");
+	Page.save();
 	return Page;
 };
 
@@ -63,8 +67,10 @@ export const updatePage = async (
 	linkCount?: number
 ): Promise<Page> => {
 	const Page = await getPage(owner);
+	if (!Page) throw new Error("invalid page");
+
 	if (theme) Page.theme = theme;
 	if (linkCount) Page.linkCount = linkCount;
-	Page.save()
+	Page.save();
 	return Page;
 };
