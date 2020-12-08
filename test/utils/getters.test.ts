2aae6f0e35220dffecd3cf92aa4dead6f951be64
David Snyder <davidgraygs4@gmail.com>
2020-12-08 13:34:12 -0500

    began adding getter tests

---
+++ test/utils/getters.test.ts
@@ -0,0 +1,8 @@
+import {getAuthSecret, getPage} from "../../src/utils/functions/getters"
+
+import dotenv from "dotenv"
+dotenv.config()
+
+test("gets auth token secret from process.env", () => {
+    expect(getAuthSecret()).toBeDefined()
+})
