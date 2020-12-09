2d85b3114d9977e5f1bda9ae67d8e4744f3f1005
David Snyder <davidgraygs4@gmail.com>
2020-12-09 08:15:27 -0500

    uncomment the other api test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import Controller from "../src/mongoose"
+import Controller from "../src/mongoose";
 import app from "../src/app";
 
 const uri = process.env.ATLAS_URI;
@@ -8,38 +8,36 @@ if (!uri) {
 	throw new Error("Missing mongoose connection string");
 }
 
-const mongoose = new Controller(uri)
+const mongoose = new Controller(uri);
 
 beforeAll(async () => {
-	await mongoose.connect()
-})
+	await mongoose.connect();
+});
 
 afterAll(async () => {
-	await mongoose.disconnect()
-})
+	await mongoose.disconnect();
+});
 
 test("responds with a not found message", async () => {
-	// expect(true).toBe(true)
-	// done()
 	await request(app)
 		.get("/what-is-this-even")
-		// .set("Accept", "application/json")
-		// .expect("Content-Type", /json/)
+		.set("Accept", "application/json")
+		.expect("Content-Type", /json/)
 		.expect(404);
 });
 
-// describe("GET /", () => {
-// 	it("responds with a json message", done => {
-// 		request(app)
-// 			.get("/")
-// 			.set("Accept", "application/json")
-// 			.expect("Content-Type", /json/)
-// 			.expect(
-// 				200,
-// 				{
-// 					message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
-// 				},
-// 				done
-// 			);
-// 	});
-// });
+describe("GET /", () => {
+	it("responds with a json message", done => {
+		request(app)
+			.get("/")
+			.set("Accept", "application/json")
+			.expect("Content-Type", /json/)
+			.expect(
+				200,
+				{
+					message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+				},
+				done
+			);
+	});
+});
