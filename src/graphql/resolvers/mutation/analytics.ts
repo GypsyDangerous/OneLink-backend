79a1f8053268d64a94dc402ebc0b1c028332007b
David Snyder <davidgraygs4@gmail.com>
2020-12-09 10:49:34 -0500

    add update analytics mutation

---
+++ src/graphql/resolvers/mutation/analytics.ts
@@ -0,0 +1,33 @@
+import User from "../../../models/User.model";
+import { DocumentQuery } from "mongoose";
+import { hasUniqueEmail, login, register, setRefreshToken } from "../../../utils/functions";
+import { Context } from "../../../types/Request";
+import { Doc } from "prettier";
+import Analytics from "../../../models/Analytics.model";
+
+export const analytics = {
+	updateAnalytics: async (
+		parent: unknown,
+		{ newAnalytics: { sessions, uniqueVisitors, clicks, links } }: { newAnalytics: Analytics },
+		context: Context
+	): DocumentQuery<Analytics | null, Analytics, unknown> => {
+		const { id } = context;
+		if (!id) throw new Error("Unauthorized");
+		const analyticsToModify = await Analytics.findOne({ owner: id });
+
+		if (!analyticsToModify) throw new Error("not found");
+
+		if (sessions) analyticsToModify.sessions = sessions;
+		if (uniqueVisitors) analyticsToModify.uniqueVisitors = uniqueVisitors;
+		if (clicks) analyticsToModify.clicks = clicks;
+		if (links) {
+			analyticsToModify.links = links;
+            analyticsToModify.markModified("links")
+        }
+		analyticsToModify.clickThroughRate =
+			(analyticsToModify.clicks / analyticsToModify.sessions) * 100;
+
+		await analyticsToModify.save();
+		return analyticsToModify;
+	},
+};
