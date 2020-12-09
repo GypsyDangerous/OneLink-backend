750247045d8a947a5f5b951a26e8806252f5737a
David Snyder <davidgraygs4@gmail.com>
2020-12-09 11:41:48 -0500

    add analytics mutation resolver to global export

--- src/graphql/resolvers/mutation/index.ts
+++ src/graphql/resolvers/mutation/index.ts
@@ -1,9 +1,11 @@
 import { auth } from "./auth";
 import { user } from "./user";
 import { page } from "./page";
+import {analytics} from "./analytics"
 
 export const Mutation = {
 	...auth,
 	...user,
 	...page,
+	...analytics
 };
