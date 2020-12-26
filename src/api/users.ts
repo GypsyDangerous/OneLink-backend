2b3ef71fb647f0674d67304198882e8dc437c7e1
David Snyder <davidgraygs4@gmail.com>
2020-12-25 21:07:32 -0500

    add customization to qr code

--- src/api/users.ts
+++ src/api/users.ts
@@ -6,30 +6,44 @@ import { updateUser } from "../utils/functions";
 const router = Router();
 import QRCode from "qrcode";
 
-router.get("/qr/:username", async (req, res) => {
-	const {username} = req.params
-	const user = await User.findOne({username})
-	if(!user){
-		return res.status(404).json({message: "User not found", code: 404})
+router.get("/qr/:username", async (req, res, next) => {
+	const { username } = req.params;
+	const user = await User.findOne({ username });
+	if (!user) {
+		return res.status(404).json({ message: "User not found", code: 404 });
 	}
-	QRCode.toBuffer(`https://www.onelinkapp.xyz/${req.params.username}`, (err, buffer) => {
-		res.setHeader("content-type", "image/png");
-		res.write(buffer, "binary");
-		res.end(null, "binary");
-	});
-});
+	try{
 
+		QRCode.toBuffer(
+			`https://www.onelinkapp.xyz/${req.params.username}`,
+			{
+				width: Number(req.query.width || 144),
+				color: {
+					light: String(req.query.light || "#ffffff"),
+					dark: String(req.query.dark || "#000000"),
+				},
+			},
+			(err, buffer) => {
+				res.setHeader("content-type", "image/png");
+				res.write(buffer, "binary");
+				res.end(null, "binary");
+			}
+		);
+	}catch(err){
+		next(err)
+	}
+});
 
 router.use(checkAuth);
 
 router.post("/force_logout", async (req, res) => {
-	const id = req.userData.userId
-	const user = await User.findById(id)
-	if(!user) return res.status(400).json({code: 400, message: "user not found"})
-	user.tokenVersion += 1
-	user.save()
-	res.json({code: 200, message: "logout succeeded"})	
-})
+	const id = req.userData.userId;
+	const user = await User.findById(id);
+	if (!user) return res.status(400).json({ code: 400, message: "user not found" });
+	user.tokenVersion += 1;
+	user.save();
+	res.json({ code: 200, message: "logout succeeded" });
+});
 
 router.get("/@me", async (req, res, next) => {
 	try {
@@ -41,11 +55,10 @@ router.get("/@me", async (req, res, next) => {
 	}
 });
 
-
 router.delete("/delete/@me", async (req, res) => {
 	try {
 		await User.findOneAndDelete({ uuid: req.userData.userId });
-		res.json({code: 200, message: "User Deleted"});
+		res.json({ code: 200, message: "User Deleted" });
 	} catch (err) {
 		res.status(500).json({ code: 500, message: "Error: " + err.message });
 	}
