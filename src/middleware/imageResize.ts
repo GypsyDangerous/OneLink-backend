ff22c9a7741f260e626d1540d0ada336d69c07b6
David Snyder <davidgraygs4@gmail.com>
2020-12-30 17:27:12 -0500

    update image resize fetching with long term caching

--- src/middleware/imageResize.ts
+++ src/middleware/imageResize.ts
@@ -7,22 +7,48 @@ import { get_url_extension } from "../utils/functions/getters";
 import mime from "mime-types";
 const promises = fs.promises;
 
+const widthAndHeightToString = (width?: string, height?: string): string => {
+	if (width && height) return `${width}x${height}`;
+	if (width) return widthAndHeightToString(width, width);
+	if (height) return widthAndHeightToString(height, height);
+	return "";
+};
+
 const imageResize: MiddlewareFunction = async (req, res, next) => {
 	try {
 		const { width, height } = req.query;
 		const pathToFetch = upload_path.substr(2);
-		console.log(path.join(pathToFetch, req.path));
 		const ext = get_url_extension(req.path);
+
 		if (!ext) throw new Error();
 		const content_type = mime.lookup(ext);
 		if (!content_type) throw new Error();
-		const file = await promises.readFile(path.join(pathToFetch, req.path));
+		res.setHeader("content-type", content_type);
+
+		const sizeText = widthAndHeightToString(width as string, height as string);
+
+		const filePath = path.join(pathToFetch, req.path)
+
+		const sizedPath = `${filePath.replace(`.${ext}`, "")}-${sizeText}.${ext}`;
+
+		if (fs.existsSync(sizedPath)) {
+			const file = await promises.readFile(sizedPath);
+			res.write(file, "binary");
+			res.end(null, "binary");
+			return
+		}
+		
+		const file = await promises.readFile(filePath);
 		const resizedImage = sharp(file);
+
 		if (width) resizedImage.resize({ width: Number(width) });
 		if (height) resizedImage.resize({ height: Number(height) });
-		res.setHeader("content-type", content_type);
+
+		promises.writeFile(sizedPath, await resizedImage.toBuffer())
+		
 		res.write(await resizedImage.toBuffer(), "binary");
 		res.end(null, "binary");
+
 	} catch (err) {
 		res.status(404).json({ message: "image not found" });
 	}
