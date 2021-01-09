f9b8cbf6751a6b7086fbbd76ef1498f7d9507d81
David Snyder <davidgraygs4@gmail.com>
2021-01-08 19:24:40 -0500

    move page creation function to seperate file

--- src/graphql/resolvers/mutation/page.ts
+++ src/graphql/resolvers/mutation/page.ts
@@ -4,17 +4,14 @@ import { Page as PageType, Link, Theme } from "../../../types/Page";
 import User from "../../../models/User.model";
 import { Context } from "../../../types/Request";
 
+import {createPage as makePage} from "../../../utils/functions/page"
+
 export const page = {
 	createPage: async (parent: unknown, args: unknown, context: Context): Promise<PageType> => {
 		const { id } = context;
 		if (!id) throw new Error("Unauthorized");
 
-		// IMPORTANT DocumentQuery is like a promise
-		const owner = await User.findById(id);
-		if (!owner) throw new Error("Unauthorized");
-		const newPage = new Page({ owner: owner.username });
-		await newPage.save();
-		return newPage;
+		return makePage(id)
 	},
 	addLink: async (
 		parent: unknown,
---
+++ src/utils/functions/page.ts
@@ -0,0 +1,14 @@
+import Page from "../../models/Page.model";
+import {Page as PageType} from "../../types/Page"
+import User from "../../models/User.model";
+
+
+export const createPage = async (id: string) : Promise<PageType> => {
+		if (!id) throw new Error("Unauthorized");
+
+		const owner = await User.findById(id);
+		if (!owner) throw new Error("Unauthorized");
+		const newPage = new Page({ owner: owner.username });
+		await newPage.save();
+		return newPage;
+}
